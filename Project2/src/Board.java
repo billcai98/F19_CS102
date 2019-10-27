432d65ddaab79e0fc13aeec534a9efb4510a8ae2
billcai98 <!@#$Bc980923>
2019-10-27 02:45:48 -0400

    c1

--- Project2/src/Board.java
+++ Project2/src/Board.java
@@ -2,18 +2,18 @@
 public class Board implements Cloneable {
 
     int[][] boardList;
-    int row;
-    int col;
+    int row_num;
+    int col_num;
 
     // Board Constructor
     Board(int[][] boardList) {
 
-        this.row = boardList.length;
-        this.col = boardList[0].length;
+        this.row_num = boardList.length;
+        this.col_num = boardList[0].length;
 
-        this.boardList = new int[row][col];
-        for (int i = 0; i<row; i++) {
-            for (int j = 0; j<col; j++) {
+        this.boardList = new int[row_num][col_num];
+        for (int i = 0; i < row_num; i++) {
+            for (int j = 0; j < col_num; j++) {
                 this.boardList[i][j] = boardList[i][j];
             }
         }
@@ -21,33 +21,45 @@ public class Board implements Cloneable {
 
     // Print Method
     public void printBoard() {
-        for (int i = 0; i<row; i++) {
-            for (int j = 0; j<col; j++) {
+        for (int i = 0; i < row_num; i++) {
+            for (int j = 0; j < col_num; j++) {
                 System.out.print(boardList[i][j] + " ");
             }
             System.out.println();
         }
     }
 
-    // Public Methods
+    // Access Methods
     public boolean isFull(){
         boolean empty = true;
-        for (int i1 = 0; i1 < row; i1++) {
-            for (int i2 = 0; i2 < col; i2++) {
+        for (int i1 = 0; i1 < row_num; i1++) {
+            for (int i2 = 0; i2 < col_num; i2++) {
                 if (boardList[i1][i2] == 0) {empty = false; break;}
             }
         }
         return empty;
     }
 
+    public int columnTopIndex(int input_col){
+        for(int index = row_num-1; index >= 0; index--) {
+            if (boardList[index][input_col] == 0) {return index;}
+        }
+        return -1;
+    }
+
+    public int get(int row, int col) {
+        return boardList[row][col];
+    }
+
+
 
     // Update Methods
     public Object clone() throws CloneNotSupportedException {
 
         Board cloneBoard = (Board) super.clone();
-        int[][] cloneList = new int[row][col];
-        for (int i = 0; i<row; i++) {
-            for (int j = 0; j<col; j++) {
+        int[][] cloneList = new int[row_num][col_num];
+        for (int i = 0; i<row_num; i++) {
+            for (int j = 0; j<col_num; j++) {
                 cloneList[i][j] = boardList[i][j];
             }
         }
--- Project2/src/MainDriver.java
+++ Project2/src/MainDriver.java
@@ -1,25 +1,82 @@
 public class MainDriver {
 
-    static final int X=1, O=2;
+    static final int R=1, B=2;
+    static final int NUM_COLUMNS = 4;
+    static final int NUM_ROWS=4;
+    static final int NUM_TO_WIN=4;
+
 
     public static void main(String[] args) throws CloneNotSupportedException {
 
-        int[][] boardList = {{1, 2, 3},
-                             {3, 4, 5},
-                             {5, 6, 7},
-                             {8, 9, 10}};
+        int[][] boardList = {{2, 2, 2, 1},
+                             {1, 1, 0, 1},
+                             {2, 2, 1, 2},
+                             {1, 2, 2, 1}};
 
         Board myBoard = new Board(boardList);
 
-
+        System.out.println(checkBoard(myBoard, 1));
     }
 
     public static void play(Board boart, int clr) {
     }
 
+    /**
+     * Method 1     :  CHECK BOARD
+     * Description  :  Return 0 if the board is full, 1 if R wins, 2 if B wins, 3 if game continues
+     */
+    public static int checkBoard(Board board, int last_plr) {
 
-    public static int checkBoard(Board board, int clr) {
-        return 1;
+        // First--Check Rows
+        for (int row = 0; row < NUM_ROWS; row++) {
+            int consecutive = 0;                                            // New Row, Set Counter to 0
+            for (int col = 0; col < NUM_COLUMNS; col++) {                   // Loop over columns
+                if (board.get(row, col) == last_plr) {
+                    consecutive++;
+                    if (consecutive == NUM_TO_WIN) {return last_plr;}       // If win, return "last player"
+                } else {
+                    consecutive = 0;                                        // If not equal, then consecutive ends. Reset Counter
+                }
+            }
+        }
+
+        // Second--Check Columns
+        for (int col = 0; col < NUM_COLUMNS; col++) {
+            int consecutive = 0;                                            // New Column, Set Counter to 0
+            for (int row = 0; row < NUM_ROWS; row++) {                      // Loop over rows
+                if (board.get(row, col) == last_plr) {
+                    consecutive++;                                          // If matched, counter++
+                    if (consecutive == NUM_TO_WIN) {return last_plr;}       // If win, return "last player"
+                } else {
+                    consecutive = 0;
+                }
+            }
+        }
+
+        // Third--Check Diagonals
+        int consecutive = 0;
+        for (int pivot = 0; pivot < NUM_COLUMNS; pivot++ ) {
+            if (board.get(pivot, pivot) == last_plr) {
+                consecutive++;
+                if (consecutive == NUM_TO_WIN)  {return last_plr;}
+            }  else {
+                consecutive = 0;
+            }
+        }
+
+        consecutive = 0;
+        for (int pivot = 0; pivot < NUM_COLUMNS; pivot++ ) {
+            if (board.get(NUM_COLUMNS-1-pivot, pivot) == last_plr) {
+                consecutive++;
+                if (consecutive == NUM_TO_WIN)  {return last_plr;}
+            }  else {
+                consecutive = 0;
+            }
+        }
+
+        // Last--Check "full" or "continue"
+        if (board.isFull()) {return 0;}
+        return 3;
     }
 
 }
